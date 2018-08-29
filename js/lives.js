ca3fedf9ef2ce1d15cb2fe380c07659bb462c5e0
Alevtine <mirada.insolente@gmail.com>
2018-08-29 16:31:14 +0300

    comments deleted

--- js/lives.js
+++ js/lives.js
@@ -6,11 +6,3 @@ export default function lives(data) {
 
   return node;
 }
-
-/*
-<div class="game__lives">
-  <img src="img/heart__empty.svg" class="game__heart" alt=" Missed Life" width="31" height="27">
-  <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
-  <img src="img/heart__full.svg" class="game__heart" alt="Life" width="31" height="27">
-</div>`;
-*/
